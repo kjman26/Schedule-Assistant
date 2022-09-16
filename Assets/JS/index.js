0aa37910529bb362d6a9d88afb1407c5a1aa675c
Chris <kjman26@gmail.com>
2022-09-15 19:16:31 -0700

    working through setting data to local storage

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,11 +1,35 @@
 //current date and time
-var time = moment().format("MMMM Do, YYYY, hh:mm a")
+var time = moment().format("MMMM Do, YYYY, HH:mm")
 $("#timeStamp").text(time)
 
 
-var eventSlots = document.querySelectorAll("events")
-eventSlots.forEach(eventSlots, function(){
-    if (eventSlots.dataset.number<time){
-        console.log('late')
-    }
-})
+// set user input to local storage
+
+
+
+
+var saveBtn = document.getElementById('#save')
+
+
+//   when i click save, move the text content (aka value) to local storage
+var userEvent = document.querySelector("#events").value;
+function save(){
+ saveBtn.addEventListener("click",function(event){
+    event.preventDefault();     
+    localStorage.setItem("userEventList", JSON.stringify(userEvent));
+        
+  });
+};
+    
+console.log(userEvent); 
+
+//get events
+
+
+//color-coding
+
+
+
+
+//does .value = the text from the input box?
+//why is value saying undefined?
