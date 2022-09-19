694f25e9f36e4c94e1d341ef319aa446b5e9bedb
Chris <kjman26@gmail.com>
2022-09-19 14:55:03 -0700

    updating functions

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,45 +1,66 @@
+$(document).ready(function(){
+
+    var time = moment().format("MMMM Do, YYYY, HH:mm")
+    $("#timeStamp").text(time)
+
+})
 //current date and time
-var time = moment().format("MMMM Do, YYYY, HH:mm")
-$("#timeStamp").text(time)
+
 
 
 // set user input to local storage
 var userInput = document.getElementsByClassName(".events");
-function saveGame(){
-   if(userInput.value){
-   userInput.textContent = 
-   localStorage.setItem("eventList", userInput.value);
-   console.log(userInput.value)}
-};
 
-var save = document.querySelectorAll('.save');
-function eventHandler(){
-    for (i=0; i<save.length; i++){
-    save[i].addEventListener("click", saveGame(i))
-};
-};
+function saveEvent(){
+    for (i=0; i<userInput.length; i++){
+     if(userInput[i].value){
+    localStorage.setItem("eventList", userInput[i].value);
+    $(".one").text = userInput[i]
+    console.log(userInput[i].value)}
+    };
+ };
+
+ $(".save").on("click",saveEvent);
 
 
 
-
-
-
-
-
-//   when i click save, move the text content (aka value) to local storage
-
-
-
-
-    
-// ; 
 
 //get events
-
+function getEvents(){
+    localStorage.getItem("eventList")
+};
+getEvents
 
 //color-coding
 
+function hourTracker() {
+    //get current number of hours.
+    var currentHour = moment().hour(); // use of moment.js
 
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        //check if we've moved past this time, click into css/html given classes of past, present, or future
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker(); //re-run function
 
 
 //does .value = the text from the input box?
--- Assets/disregard.js
+++ Assets/disregard.js
@@ -1,19 +1,69 @@
-var save = document.querySelectorAll('.save');
+$(document).ready(function(){
 
-var userInput = document.querySelectorAll(".one");
+$('.saveBtn').on("click", function(){
+    console.log(this);
+       
+    var userEvent = $(".one").sibling()
+    var time = $(this).parent().attr("id");
+    
+    localStorage.setItem(time, JSON.stringify(userEvent));
+   console.log(userEvent.value);
+});
+});
+
+$("#nineAM .one").val(localStorage.getItem("nineAM"));
+$("#tenAM .one").val(localStorage.getItem("tenAM"));
+$("#elevenAM .one").val(localStorage.getItem("elevenAM"));
+$("#twelvePM .one").val(localStorage.getItem("twelvePM"));
+$("#onePM .one").val(localStorage.getItem("onePM"));
+$("#twoPM .one").val(localStorage.getItem("twoPM"));
+$("#threePM .one").val(localStorage.getItem("threePM"));
+$("#fourPM .one").val(localStorage.getItem("fourPM"));
+$("#fivePM .one").val(localStorage.getItem("fivePM"));
 
 
-function saveEvent(){
-   for (i=0; i<userInput.length; i++){
-    if(userInput[i].value){
-   localStorage.setItem("eventList", userInput[i].value);
-   console.log(userInput[i].value)}
+// var userInput = document.querySelectorAll("one");
+var divs = document.querySelectorAll(".divs")
+function hourTracker() {
+    //get current number of hours.
+    var currentHour = moment().hour(); // use of moment.js
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        //check if we've moved past this time, click into css/html given classes of past, present, or future
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
 }
-};
+hourTracker(); //re-run function
+// function saveEvent(){
+//    for (i=0; i<userInput.length; i++){
+//    localStorage.setItem("eventList", userInput[i].value);
+//    console.log(userInput[i].value)}
+// };   
+// });
+// if(userInput.id === $('.save').dataset){
+//  divs.forEach($(".save")).on("click", saveEvent); 
+// }
+// else{
+//     console.log('wrong')
+// };
 
-save.forEach(save => save.addEventListener('click', saveEvent())
-);
 // save.addEventListener("click", saveGame);
 // console.log(userInput.value)
-
-
